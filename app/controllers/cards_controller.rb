5f9fcc05d3ea7503c4665a00202d4c1e41fdef2e
aaidenplays <aaidenplays@yahoo.com>
2020-04-16 13:32:08 -0500

    can pull img from api

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,22 @@
 class CardsController < ApplicationController
+    def index 
+        @cards = Card.all
+        render json: @cards
+    end
+
+    def show 
+        @card = Card.find_by(id: params[:id])
+        render json: @card
+    end
+
+    def create
+        @card = Card.create(card_params)
+        render json: @card
+    end
+
+    private
+
+    def card_params
+        params.require(:card).permit(:imgurl)
+    end
 end
