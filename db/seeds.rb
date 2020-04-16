95ddeacbc2e1765ceb1c2137e09770cfd769461b
jbshipman <jbshipman@icloud.com>
2020-04-16 14:11:09 -0500

    seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,30 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Card.destroy_all
 
-Card.create(imgurl: 'http://localhost:3001/images/AntonioReid.png')
+cards = [
+  {imgurl: 'http://localhost:3001/images/AntonioReid.png'},
+  {imgurl: 'http://localhost:3001/images/BenBrower.png'},
+  {imgurl: 'http://localhost:3001/images/BenSullivan.png'},
+  {imgurl: 'http://localhost:3001/images/ChanceMims.png'},
+  {imgurl: 'http://localhost:3001/images/DaneDawson.png'},
+  {imgurl: 'http://localhost:3001/images/DougHarrison.png'},
+  {imgurl: 'http://localhost:3001/images/EdwinKim.png'},
+  {imgurl: 'http://localhost:3001/images/FrancoCanedo.png'},
+  {imgurl: 'http://localhost:3001/images/GavinWhitlock.png'},
+  {imgurl: 'http://localhost:3001/images/JackParker.png'},
+  {imgurl: 'http://localhost:3001/images/JamesShipman.png'},
+  {imgurl: 'http://localhost:3001/images/JessicaTriana.png'},
+  {imgurl: 'http://localhost:3001/images/JoelleHelm.png'},
+  {imgurl: 'http://localhost:3001/images/JulianaStackland.png'},
+  {imgurl: 'http://localhost:3001/images/LaurenBauml.png'},
+  {imgurl: 'http://localhost:3001/images/LughWilson.png'},
+  {imgurl: 'http://localhost:3001/images/MarcMajcher.png'},
+  {imgurl: 'http://localhost:3001/images/MattBritt.png'},
+  {imgurl: 'http://localhost:3001/images/MatthewJagiello.png'},
+  {imgurl: 'http://localhost:3001/images/MaylenePoulsen.png'},
+  {imgurl: 'http://localhost:3001/images/PatrickKennedy.png'},
+  {imgurl: 'http://localhost:3001/images/ReeseBowman.png'}
+]
+cards.each {|card| Card.create(card)}
