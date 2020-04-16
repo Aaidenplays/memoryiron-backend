1554d5f0b11169447aa795caaf71cf94cdf66bea
aaidenplays <aaidenplays@yahoo.com>
2020-04-16 13:57:15 -0500

    url template for photos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Card.create(imgurl: 'http://localhost:3001/images/AntonioReid.png')
